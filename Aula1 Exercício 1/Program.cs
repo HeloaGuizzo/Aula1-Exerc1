729c0ca6c09bf6b9230c978673c8a2109a7e92b7
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 16:53:11 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1 Exercício 1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Heloá Guizzo";
+            string rua = "Rua Alarico Correa Leite";
+            int numero = 96;
+            int cep = 12446230;
+            int telefone = 991212030;
+
+            Console.WriteLine($"Nome: {nome}\nEndereço: {rua}, {numero}\nCEP: {cep}\n "+
+                $"Telefone: {telefone}");
+        }
+    }
+}
